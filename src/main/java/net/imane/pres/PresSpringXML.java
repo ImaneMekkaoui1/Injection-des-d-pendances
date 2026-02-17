953ed417036b5d774add52b0977c46c726828d11
ImaneMekkaoui <mekkaouiimane722@gmail.com>
2026-02-17 21:49:50 +0000

    feat: dependency injection using Spring XML configuration

---
+++ src/main/java/net/imane/pres/PresSpringXML.java
@@ -0,0 +1,19 @@
+package net.imane.pres;
+
+import net.imane.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Imane Mekkaoui
+ */
+public class PresSpringXML {
+    public static void main(String[] args){
+        ApplicationContext springContext =
+                new ClassPathXmlApplicationContext("config.xml");
+
+        IMetier metier = (IMetier) springContext.getBean("metier");
+        System.out.println("RES = "+metier.Calcul());
+
+    }
+}
